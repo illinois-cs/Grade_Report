bfe6121b5d18a4b9ec191673ac2f7e84eea345d0
bchong95 <bschong2@illinois.edu>
2015-09-07 16:12:19 +0000

    did some basic layout

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,6 @@
 var gradeReportControllers = angular.module('gradeReportControllers', []);
-gradeReportControllers.controller('MainController', ['$scope' , '$window' , function($scope, $window) {
-  
+gradeReportControllers.controller('MainController', ['$scope' , '$http' , function($scope, $http) {
+  $http.get('./data/results.json').success(function(data) {
+   $scope.results = data;
+ });
 }]);
