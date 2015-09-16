42b7e8955f3b026d861dd97dd83df8b6a4130ef0
bchong95 <bschong2@illinois.edu>
2015-09-16 20:35:00 +0000

    get rid of it all

--- js/controllers.js
+++
@@ -1,6 +0,0 @@
-var gradeReportControllers = angular.module('gradeReportControllers', []);
-gradeReportControllers.controller('MainController', ['$scope' , '$http' , function($scope, $http) {
-  $http.get('./data/results.json').success(function(data) {
-   $scope.results = data;
- });
-}]);
