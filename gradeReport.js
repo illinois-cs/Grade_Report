98a1f11b508fa9dbb4238a16dabd76c011cebe06
Brandon Chong <bschong2@illinois.edu>
2015-12-21 09:37:39 +0000

    need to finish this up at some point

--- gradeReport.js
+++ gradeReport.js
@@ -4,6 +4,7 @@ gradeReportControllers.controller('MainController', ['$scope', '$http', function
   $http
     .get("results.json").success(function(data) {
       $scope.results = data;
+      // $scope.selected = $scope.results[$scope.results-1].timestamp;
       /*!
   		Paper Collapse v0.4.0
 
@@ -17,5 +18,8 @@ gradeReportControllers.controller('MainController', ['$scope', '$http', function
   			(function(){!function(e){"use strict";e.fn.paperCollapse=function(o){var a;return a=e.extend({},e.fn.paperCollapse.defaults,o),e(this).find(".collapse-card__heading").add(a.closeHandler).click(function(){e(this).closest(".collapse-card").hasClass("active")?(a.onHide.call(this),e(this).closest(".collapse-card").removeClass("active"),e(this).closest(".collapse-card").find(".collapse-card__body").slideUp(a.animationDuration,a.onHideComplete)):(a.onShow.call(this),e(this).closest(".collapse-card").addClass("active"),e(this).closest(".collapse-card").find(".collapse-card__body").slideDown(a.animationDuration,a.onShowComplete))}),this},e.fn.paperCollapse.defaults={animationDuration:400,easing:"swing",closeHandler:".collapse-card__close_handler",onShow:function(){},onHide:function(){},onShowComplete:function(){},onHideComplete:function(){}}}(jQuery)}).call(this);
   			$('.collapse-card').paperCollapse();
   		};
+      // console.log("hello");
+      // (function(){!function(e){"use strict";e.fn.paperCollapse=function(o){var a;return a=e.extend({},e.fn.paperCollapse.defaults,o),e(this).find(".collapse-card__heading").add(a.closeHandler).click(function(){e(this).closest(".collapse-card").hasClass("active")?(a.onHide.call(this),e(this).closest(".collapse-card").removeClass("active"),e(this).closest(".collapse-card").find(".collapse-card__body").slideUp(a.animationDuration,a.onHideComplete)):(a.onShow.call(this),e(this).closest(".collapse-card").addClass("active"),e(this).closest(".collapse-card").find(".collapse-card__body").slideDown(a.animationDuration,a.onShowComplete))}),this},e.fn.paperCollapse.defaults={animationDuration:400,easing:"swing",closeHandler:".collapse-card__close_handler",onShow:function(){},onHide:function(){},onShowComplete:function(){},onHideComplete:function(){}}}(jQuery)}).call(this);
+      // $('.collapse-card').paperCollapse();
     });
 }]);
